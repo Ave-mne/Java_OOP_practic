13527ac7d91055deb14f27b8a1d13a67721894a1
«Valeev_Andrey» <Valeevptk@yandex.ru>
2023-05-30 13:33:39 +0300

    Решено ДЗ

---
+++ src/main/java/Seminar1/Main.java
@@ -0,0 +1,47 @@
+package Seminar1;
+
+public class Main {
+    public static void main(String[] args) {
+        CoffeeMachine coffeeAutomate = new CoffeeMachine();
+        coffeeAutomate.addCoffee(new Coffee("Americano", 50, 150, 70, 10))
+                .addCoffee(new Coffee("Capuchino", 60, 100, 75, 5))
+                .addCoffee(new Coffee("Latte", 80, 150, 80, 10))
+                .addCoffee(new Coffee("Nescafe", 40, 200,90, 5));
+
+        System.out.println("Включение...");
+        System.out.println(coffeeAutomate);
+
+        System.out.println("-------------------");
+        System.out.println(coffeeAutomate);
+
+        System.out.println("-------------------");
+
+        System.out.println("Выберите напиток:");
+        PrintSell(coffeeAutomate,"Nescafe");
+
+        System.out.println("-------------------");
+
+        System.out.println("Выберите напиток:");
+        PrintSell(coffeeAutomate,"Americano");
+
+
+
+        System.out.println("-------------------");
+
+        System.out.println("Выберите напиток: ");
+        PrintSell(coffeeAutomate,"Цикорий");
+
+
+        System.out.println("-------------------");
+        System.out.println(coffeeAutomate);
+        System.out.println("Выключение...");
+    }
+
+    public static void PrintSell (CoffeeMachine machine, String nameCafe){
+        try{
+            System.out.println(machine.sell(nameCafe));
+        } catch (Exception e) {
+            System.out.println("Товар не найден");
+        }
+    }
+}
