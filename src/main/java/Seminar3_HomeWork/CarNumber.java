9288ae0aa51a2a3fd0809eb91804b98521bcfd92
«Valeev_Andrey» <Valeevptk@yandex.ru>
2023-06-05 18:03:25 +0300

    Решено ДЗ

---
+++ src/main/java/Seminar3_HomeWork/CarNumber.java
@@ -0,0 +1,39 @@
+package Seminar3_HomeWork;
+
+public class CarNumber {
+    private String number;
+
+    private CarNumber next;
+    private CarNumber previous;
+
+    public CarNumber(String number) {
+        this.number = number;
+        this.next = null;
+        this.previous = null;
+    }
+
+    @Override
+    public String toString() {
+        return "CarNumber = " + number;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public CarNumber getNext() {
+        return next;
+    }
+
+    public CarNumber getPrevious() {
+        return previous;
+    }
+
+    public void setNext(CarNumber next) {
+        this.next = next;
+    }
+
+    public void setPrevious(CarNumber previous) {
+        this.previous = previous;
+    }
+}
--- src/main/java/Seminar3_HomeWork/Main.java
+++ src/main/java/Seminar3_HomeWork/Main.java
@@ -1,4 +1,21 @@
 package Seminar3_HomeWork;
 
 public class Main {
+    public static void main(String[] args) {
+        Parking parking = new Parking();
+        parking.addCar(new CarNumber("001"));
+        parking.addCar(new CarNumber("002"));
+        parking.addCar(new CarNumber("003"));
+
+        for (CarNumber carNumber : parking) {
+            System.out.println(carNumber);
+        }
+
+        System.out.println("---------------");
+        parking.addFirstCar(new CarNumber("999"));
+        for (CarNumber carNumber : parking) {
+            System.out.println(carNumber);
+        }
+
+    }
 }
